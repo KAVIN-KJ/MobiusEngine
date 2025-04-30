171a86e5efbba334e1c31597bd9f803b68272cfb
KAVIN-KJ <kavinkj2004@gmail.com>
2025-05-01 01:22:36 +0530

    edit

--- src/AdvancedPlan.jsx
+++ src/AdvancedPlan.jsx
@@ -8,7 +8,6 @@ const AdvancedPlan = () => {
       <div className="advance-left">
         <h2>Advance</h2>
         <p className="subtitle">Top-tier support for serious job hunters:</p>
-        <div className='divider'></div>
         <ul className="features-grid">
           <li><img src={check} /> Everything in Plus</li>
           <li><img src={check} /> Custom Resumes & Cover Letters</li>
